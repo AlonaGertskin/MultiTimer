b4321db61b3bf41dd985385e1a7d9b31981fc5eb
Alona Gertskin <alonagertskin@gmail.com>
2026-03-26 12:55:57 +0200

    connected a button to the timer cards using inkwell ripple

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'timer_card.dart';
 import 'timer_model.dart';
+import 'dart:async'; //contains the Timer class
 
 class MyMainPage extends StatefulWidget {
   const MyMainPage({super.key});
@@ -14,18 +15,30 @@ class _MyMainPageState extends State<MyMainPage> {
     TimerModel(title: 'Pasta', remainingSeconds: 600),
     TimerModel(title: 'Egg', remainingSeconds: 300),
   ];
-@override
+
+  void startTimer(TimerModel timer) {
+    // For now, we just print to the console to make sure the "link" works
+    print('Starting timer for: ${timer.title}');
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Multi-Timer')),
-      body: ListView.builder(
-        itemCount: timers.length,
-        itemBuilder: (context, index) {
-          // This function runs for every item in our 'timers' list
-          final currentTimer = timers[index];
-          return TimerCard(timer: currentTimer);
-        },
-      ),
+        appBar: AppBar(title: const Text('Multi-Timer')),
+        body: ListView.builder(
+            itemCount: timers.length,
+            itemBuilder: (context, index) {
+              // This function runs for every item in our 'timers' list
+              final currentTimer = timers[index];
+              return InkWell(
+                onTap: () {
+                  // Small step: Just call our print function
+                  startTimer(currentTimer);
+                },
+                child: TimerCard(timer: currentTimer),
+              );
+            }
+        )
     );
   }
-  }
+}

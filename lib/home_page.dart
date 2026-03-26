f285ba31ebaae305e494b62346554419f6c8d72e
Alona Gertskin <alonagertskin@gmail.com>
2026-03-26 12:31:26 +0200

    changed homepage the stateful and a timer list using the timer cards

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
 import 'timer_card.dart';
+import 'timer_model.dart';
 
-class MyMainPage extends StatelessWidget {
+class MyMainPage extends StatefulWidget {
   const MyMainPage({super.key});
 
   @override
+  State<MyMainPage> createState() => _MyMainPageState();
+}
+
+class _MyMainPageState extends State<MyMainPage> {
+  List<TimerModel> timers = [
+    TimerModel(title: 'Pasta', remainingSeconds: 600),
+    TimerModel(title: 'Egg', remainingSeconds: 300),
+  ];
+@override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Multi-Timer')),
-        body: ListView(
-            children: [
-              TimerCard(),
-              TimerCard(),
-              TimerCard(),
-            ]
-        )
+      appBar: AppBar(title: const Text('Multi-Timer')),
+      body: ListView.builder(
+        itemCount: timers.length,
+        itemBuilder: (context, index) {
+          // This function runs for every item in our 'timers' list
+          return const TimerCard();
+        },
+      ),
     );
   }
-}
+  }
